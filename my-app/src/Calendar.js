d60b1b1aa8c7d8c1498ed0a317fdb03fd5e0002d
ravenhekasol <john@hekasol.co>
2023-04-24 00:24:26 -0500

    creating calendar.js with mui.

--- my-app/src/Calendar.js
+++ my-app/src/Calendar.js
@@ -1,42 +1,140 @@
-import React from 'react';
-import { Grid, Typography } from '@mui/material';
-import './Calendar.css';
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
-function HorizontalCalendar() {
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const currentDate = new Date();
+const columns = [
+  { id: 'sunday', label: 'Sunday', minWidth: 170 },
+  { id: 'monday', label: 'Monday', minWidth: 170 },
+  { id: 'tuesday', label: 'Tuesday', minWidth: 170 },
+  { id: 'wednesday', label: 'Wednesday', minWidth: 170 },
+  { id: 'thursday', label: 'Thursday', minWidth: 170 },
+  { id: 'friday', label: 'Friday', minWidth: 170 },
+  { id: 'saturday', label: 'Saturday', minWidth: 170 },
+//   {
+//     id: 'population',
+//     label: 'Population',
+//     minWidth: 170,
+//     align: 'right',
+//     format: (value) => value.toLocaleString('en-US'),
+//   },
+//   {
+//     id: 'size',
+//     label: 'Size\u00a0(km\u00b2)',
+//     minWidth: 170,
+//     align: 'right',
+//     format: (value) => value.toLocaleString('en-US'),
+//   },
+//   {
+//     id: 'density',
+//     label: 'Density',
+//     minWidth: 170,
+//     align: 'right',
+//     format: (value) => value.toFixed(2),
+//   },
+];
 
-  // Generate an array of dates for the next 7 days
-  const dates = [...Array(7)].map((_, index) => {
-    const date = new Date(currentDate);
-    date.setDate(currentDate.getDate() + index);
-    return date;
-  });
+const rows = [];
 
-  return (
-<Grid container className="container">
-      <Grid item className="navbar-desktop">
-        <div>Navbar</div>
-      </Grid>
-      <Grid item className="extra-space">
-        <div>Extra space above calendar</div>
-      </Grid>
-      <Grid container item className="calendar">
-        {dates.map((date, index) => (
-          <Grid item key={date} className="grid-item">
-            <Typography variant="body1">{daysOfWeek[index]}</Typography>
-            <Typography variant="body1">{date.toLocaleDateString()}</Typography>
-          </Grid>
-        ))}
-      </Grid>
-      <Grid item className="navbar-tablet">
-        <div>Navbar</div>
-      </Grid>
-      <Grid item className="navbar-mobile">
-        <div>Navbar</div>
-      </Grid>
-    </Grid>
-  );
+function createData(time) {
+  return { time };
 }
 
-export default HorizontalCalendar;
+for (let i = 0; i < 24; i++) {
+  const hour = i < 10 ? `0${i}` : i;
+  rows.push(createData(`${hour}:00`));
+  rows.push(createData(`${hour}:30`));
+}
+
+// function createData(sunday, code, population, size) {
+//   const density = population / size;
+//   return { sunday, code, population, size, density };
+// }
+
+// const rows = [
+//   createData('India', 'IN', 1324171354, 3287263),
+//   createData('China', 'CN', 1403500365, 9596961),
+//   createData('Italy', 'IT', 60483973, 301340),
+//   createData('United States', 'US', 327167434, 9833520),
+//   createData('Canada', 'CA', 37602103, 9984670),
+//   createData('Australia', 'AU', 25475400, 7692024),
+//   createData('Germany', 'DE', 83019200, 357578),
+//   createData('Ireland', 'IE', 4857000, 70273),
+//   createData('Mexico', 'MX', 126577691, 1972550),
+//   createData('Japan', 'JP', 126317000, 377973),
+//   createData('France', 'FR', 67022000, 640679),
+//   createData('United Kingdom', 'GB', 67545757, 242495),
+//   createData('Russia', 'RU', 146793744, 17098246),
+//   createData('Nigeria', 'NG', 200962417, 923768),
+//   createData('Brazil', 'BR', 210147125, 8515767),
+// ];
+
+export default function StickyHeadTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
